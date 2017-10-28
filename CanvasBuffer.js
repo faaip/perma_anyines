dac0174e57e5c9442dceb1daf11ab8d9e70abd90
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2017-10-28 17:55:44 +0200

    mouse markers

--- CanvasBuffer.js
+++ CanvasBuffer.js
@@ -1,33 +1,61 @@
 function CanvasBuffer(x, y, w, h) {
   // Input arguments
-	this.x = x;
-	this.y = y;
-	this.w = w;
-	this.h = h;
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
 
-	// setup the rest
+  // setup the rest
   textFont(font);
 
   this.move = function() {
 
   };
 
-	this.resize = function(x,y,w,h){
-		// Input arguments
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-	}
+  this.resize = function(x, y, w, h) {
+    // Input arguments
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
 
-  this.display = function() {
-    translate(this.x, this.y);
-    fill(255);
-    text(str(mouseX-this.x) + "\n" + str(mouseY-this.y), 15, 15);
-    resetMatrix();
-  };
+
+
+
 
   this.isMouseInside = function() {
     return mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h;
+  }
+
+  this.display = function() {
+    this.drawText();
+    this.drawMarkers();
+  };
+
+  this.drawText = function() {
+    translate(this.x, this.y);
+    fill(255);
+    text(str(mouseX - this.x) + "\n" + str(mouseY - this.y), 15, 15);
+    resetMatrix();
+  }
+
+  this.drawMarkers = function() {
+		if(this.isMouseInside()){
+			var mWidth = 3;
+			var mLength = 15;
+			var mouseXRelative = mouseX - this.x;
+			var mouseYRelative = mouseY - this.y;
+
+			translate(this.x, this.y);
+			fill(255);
+			rect(mouseXRelative, 0, mWidth, mLength); // top
+			rect(mouseXRelative, this.h, mWidth, -mLength); // bottom
+			rect(0, mouseYRelative, mLength, mWidth); // left
+			rect(this.w, mouseYRelative, -mLength, mWidth); // right
+
+			resetMatrix();
+		}
+
   }
 }
